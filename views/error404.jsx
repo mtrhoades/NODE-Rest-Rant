89041aa8b7989ddf81b3605a2d870ffdac48ca40
root <mtrhoades0727@gmail.com>
2022-03-10 19:00:41 -1000

    Adding bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,9 +7,12 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                <img id="cat404" src="/images/cat404.jpg" alt="animated cat" />
+            </div>
           </main>
       </Def>
     )
   }
-  
+
 module.exports = error404
--- views/home.jsx
+++ views/home.jsx
@@ -6,13 +6,18 @@ const Def = require('./default')
 function home () {
     return (
       <Def>
-          <main>
-              <h1>HOME</h1>
-              <a href="/places">
+        <main>
+            <h1>HOME</h1>
+            <div>
+                <img id="fire" src="/images/fire.jpg" alt="wood fire" />
+                    <div>
+                        Photo by <a href="AUTHOR_LINK">Tirza van Dijk</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                    </div>
+            </div>
+            <a href="/places">
                 <button className="btn-primary">Places Page</button>
-                </a>
-          </main>
-      </Def>
+            </a>
+        </main>      </Def>
     )
   }
 
