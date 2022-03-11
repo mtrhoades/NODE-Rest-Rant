b08b1e939d7beb067cc204bed0ec4c1a01408e06
root <mtrhoades0727@gmail.com>
2022-03-10 15:47:45 -1000

    Adding a 404 page view

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.status(404).send(`<h1>404 Page, Not Found!</h1>`)
+    res.render('error404')
 });
 
 app.use('/places', require('./controllers/places'));
