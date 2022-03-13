3d7ccc86527fd15a3c361ac58f287076fbec7e0b
root <mtrhoades0727@gmail.com>
2022-03-13 09:07:51 -1000

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -65,9 +65,18 @@ router.get("/:id/edit", (req, res) => {
   res.send("Form page for editing an existing place");
 });
 
-router.delete("/:id", (req, res) => {
-  let id = req.params.id;
-  res.send("Delete a particular place");
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+      places.splice(id, 1);
+    res.redirect('/places')
+  }
 });
 
 router.post("/:id/rant", (req, res) => {
