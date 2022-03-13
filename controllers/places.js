29a62f3b09ad1c32c494b4a1af864121df194b3c
root <mtrhoades0727@gmail.com>
2022-03-13 08:31:01 -1000

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -42,9 +42,16 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 });
 
-router.get("/:id", (req, res) => {
-  let id = req.params.id;
-  res.send("Details about a particular place");
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+        res.render('places/show', { place: places[id] })    }
 });
 
 router.put("/:id", (req, res) => {
