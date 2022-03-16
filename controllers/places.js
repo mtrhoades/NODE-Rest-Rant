bbc4e0df682969fe3215b0e6ab23a1c88215ba52
root <mtrhoades0727@gmail.com>
2022-03-15 14:42:06 -1000

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -55,9 +55,10 @@ router.get("/:id/edit", (req, res) => {
     res.render('error404')
   }
   else {
-      res.render('places/edit', { place: places[id] });    
+    res.render("places/edit", { place: places[id], index: req.params.id });
   }
 });
+
 
 // SHOW PAGE ROUTE
 router.get('/:id', (req, res) => {
@@ -74,9 +75,31 @@ router.get('/:id', (req, res) => {
 });
 
 // UPDATE PLACE ROUTE
-router.put("/:id", (req, res) => {
-  let id = req.params.id;
-  res.send("Update a particular place");
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
 });
 
 // DELETE PLACE ROUTE
